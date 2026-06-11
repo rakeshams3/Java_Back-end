9600e25977b261e315d091b6be2729557e16736e
Rocky <rakeshams3@gmail.com>
2026-06-11 20:04:01 +0530

    learning the create a table and insert values in two learn

---
+++ SQL/SQL_Days/Day1/insert_create.sql
@@ -0,0 +1,14 @@
+1. #create table 
+  create table Student
+  (
+  Student_id int , 
+  Student_name varchar(30),
+   Student_dob date    #year/month/date
+);
+
+2. insert into Student values(1 , 'Rakesh' , '2005/03/27');          #insert values in short form 
+
+3. insert into Student ( Student_id  Student_name  , Student_dob ) values(1 , 'Rakesh' , '2005/03/27');  
+# formal way use 
+
+#but both are crt.
