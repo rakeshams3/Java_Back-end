314b130ce976b9d87b74fc9c64d1da32c098bec2
Rocky <rakeshams3@gmail.com>
2026-06-11 10:58:30 +0530

    SQL basics concept

---
+++ SQL/SQL_Days/Day1/basics.sql
@@ -0,0 +1,30 @@
+Notes
+
+Data:
+Data can be facts, statistics, audio, video or images.
+
+TYPES OF DATA
+
+Structured data:
+Collection of organized data is called as Structured data.
+
+Semi Structured data:
+Collection of partially organized data is called as Semi Structured data.
+
+Unstructured Data:
+Collection of Unorganized data is called as Unstructured data.
+
+Database
+Database is a collection of organized data
+
+Relational Database
+Relational Database is a database where data is organized in tabular structure
+
+Non-Relational Database
+Non Relational Databases do not use tabular structure to store the data, In Non Relational Databases data is stored in key:value pairs or graphs or in documents
+
+RDBMS
+Relational database Management systems/ softwares is used to manage Relational Databases
+
+NO SQL
+Non Relational Databases Management softwares are used to manage Non Relational Databases.
