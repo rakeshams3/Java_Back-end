0c1baa696614ac337ad315a1ea9914ce391f880d
Rocky <rakeshams3@gmail.com>
2026-06-11 13:27:20 +0530

    basic cmd learning in sql :create database & create table

---
+++ SQL/SQL_Days/Day1/basic_cmds.sql
@@ -0,0 +1,16 @@
+1. show databses;   #show the all databasesss
+
+2. create database University;       #create databases using the (create database)
+
+ use University;          #after createing use dabases 
+ 
+3. create table Student
+(
+S_id int ,
+S_name varchar(30) ,
+s_dob date ,                            #creating table inset attributes(colom)'s
+S_fee decimal (10,2)
+);
+
+4. desc Student;                  #show the how many attribute are there in table 
+
