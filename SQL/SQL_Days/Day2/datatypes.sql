c4ca63f67c107866a0d6b4f2ccea473c902232d1
Rocky <rakeshams3@gmail.com>
2026-06-12 18:38:56 +0530

    learning the DataTypes with examples

---
+++ SQL/SQL_Days/Day2/datatypes.sql
@@ -0,0 +1,28 @@
+Data Types
+
+1. INT
+   - Stores whole numbers
+   - Size: 4 bytes
+
+2. VARCHAR(10)
+   - Stores variable-length text
+   - Example: 'Rakesha'
+   - Uses actual characters + small overhead
+
+3. DECIMAL(5,2)
+   - Total digits = 5
+   - Digits after decimal = 2
+
+   Examples:
+   123.45   ✅
+   999.99   ✅
+   12.3     ✅ (stored as 12.30)               #digits >= decimal 
+    
+ 
+4. DATE
+   Format:
+   '2026-06-10'
+
+5. DATETIME
+   Format:
+   '2026-06-10 06:10:00'
