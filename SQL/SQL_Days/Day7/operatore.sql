54a6fec3d1a9ab243d755413cba9341abebace85
Rocky <rakeshams3@gmail.com>
2026-06-17 14:41:46 +0530

    learn comparestion and between AND

--- SQL/SQL_Days/Day7/operatore.sql
+++ SQL/SQL_Days/Day7/operatore.sql
@@ -7,3 +7,22 @@ SELECT name, marks FROM student;
 2. WHERE
 SELECT * FROM student
 WHERE marks > 80;
+
+3. Comparison Operators
+Operator	Meaning
+=	Equal
+!= or <>	Not Equal
+>	Greater Than
+<	Less Than
+>=	Greater Than or Equal
+<=	Less Than or Equal
+
+Example:
+
+WHERE age >= 21;
+
+
+4. BETWEEN
+WHERE marks BETWEEN 80 AND 90;
+
+✅ Includes both 80 and 90.
