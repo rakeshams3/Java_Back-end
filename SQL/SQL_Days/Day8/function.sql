d213f901b7493d41d512d6419b3ef9f7c485abbe
Rocky <rakeshams3@gmail.com>
2026-06-18 21:50:01 +0530

    learning the upper and lowe in sql

--- SQL/SQL_Days/Day8/function.sql
+++ SQL/SQL_Days/Day8/function.sql
@@ -17,4 +17,38 @@ flow controll
 1. string
 
 
+function it is use in select and where clause also both .
 
+📝 SQL String Functions Notes
+1. UPPER()
+Converts text to uppercase.
+
+Syntax:
+
+UPPER(column_name)
+
+Example:
+
+SELECT UPPER(name) FROM student;
+
+Output:
+
+RAKESH
+ALICE
+
+
+2. LOWER()
+Converts text to lowercase.
+
+Syntax:
+
+LOWER(column_name)
+
+Example:
+
+SELECT LOWER(name) FROM student;
+
+Output:
+
+rakesh
+alice
