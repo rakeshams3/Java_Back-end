0ba04ab45943bb662725903799e125a1903e1002
Rocky <rakeshams3@gmail.com>
2026-06-20 22:11:28 +0530

    Mod() function

---
+++ SQL/SQL_Days/Day10/number_functions.sql
@@ -0,0 +1,23 @@
+SQL Number Functions Notes (MySQL)
+1. MOD(number1, number2)
+
+Purpose: Returns the remainder after division.
+
+Syntax:
+
+MOD(number1, number2)
+
+Example:
+
+SELECT MOD(10, 3);
+
+Output:
+
+1
+
+Use Case: Checking whether a number is even or odd.
+
+SELECT MOD(8, 2);   -- 0 (Even)
+SELECT MOD(9, 2);   -- 1 (Odd)
+
+
