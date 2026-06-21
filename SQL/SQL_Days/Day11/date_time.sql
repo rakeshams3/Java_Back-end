dbbc78914d88b49c5be79212c261fbe09e1f78dd
Rocky <rakeshams3@gmail.com>
2026-06-21 22:06:03 +0530

    now and curentdate

---
+++ SQL/SQL_Days/Day11/date_time.sql
@@ -0,0 +1,25 @@
+. NOW()
+
+Purpose: Returns the current date and time.
+
+Syntax:
+
+SELECT NOW();
+
+Example Output:
+
+2026-06-21 10:30:45
+
+
+
+2. CURDATE()
+
+Purpose: Returns the current date only.
+
+Syntax:
+
+SELECT CURDATE();
+
+Output:
+
+2026-06-21
