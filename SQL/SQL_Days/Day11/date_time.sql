ad1f2bebd2ddd47d0042556d57e9efb82cfbdf6e
Rocky <rakeshams3@gmail.com>
2026-06-21 22:06:57 +0530

    systemtime

--- SQL/SQL_Days/Day11/date_time.sql
+++ SQL/SQL_Days/Day11/date_time.sql
@@ -23,3 +23,26 @@ SELECT CURDATE();
 Output:
 
 2026-06-21
+
+
+3. CURTIME()
+
+Purpose: Returns the current time only.
+
+Syntax:
+
+SELECT CURTIME();
+
+Output:
+
+10:30:45
+
+
+
+4. SYSDATE()
+
+Purpose: Returns the current system date and time at the moment the function executes.
+
+Syntax:
+
+SELECT SYSDATE();
