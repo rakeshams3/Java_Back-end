1305f72e7afbf84c58b3e1ebdc2cb6626d5097c0
Rocky <rakeshams3@gmail.com>
2026-06-28 19:12:10 +0530

    If fun() in control flow fun()

---
+++ SQL/SQL_Days/Day13/control_flow.sql
@@ -0,0 +1,22 @@
+📘 MySQL Control Flow Functions
+Definition
+
+Control Flow Functions return different values based on conditions.
+
+They work like if-else statements in programming languages.
+
+1. IF()
+Purpose
+
+Returns one value if the condition is true and another if false.
+
+Syntax
+IF(condition, value_if_true, value_if_false)
+
+Example
+
+SELECT name,
+       IF(salary>50000,'High','Low') AS Salary_Status
+FROM employee;
+
+
